f03654c3377b6748d26424be32fa9855de0f7a53
Nuri <nurikaraca89@gmail.com>
2024-09-28 17:56:51 +0300

    responsive denemeleri

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,28 @@
 import Product from "@/components/Product";
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
+import { FaRegSmileBeam } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="relative">
     {/* Video Background */}
-    <div className="relative h-screen flex justify-center items-end">
+    <div className="relative h-[300px]  lg:h-screen flex justify-center items-end">
+      <div className="absolute flex items-center justify-center gap-2 bottom-10 ">
+          <FaRegSmileBeam className="text-black text-2xl" />
+         <h2 className="text-slate-700 text-2xl">Lezzetli ve  pahalı ...</h2>
+      </div>
       <video
         autoPlay
         loop
         muted
-        className="absolute top-0 left-0 w-full h-screen object-cover z-[-100]"
+        className="hidden lg:block absolute top-0 left-0 w-full h-screen object-cover z-[-100]"
       >
         <source src="/video.mp4" type="video/mp4" />
       </video>
     </div>
 
-     <div className="relative z-10">
+     <div className="relative z-10 ">
         <Product />
       </div>
      
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import { data } from "../data.js";
 import Image from "next/image";
 const Product = () => {
   return (
-    <div className="flex flex-wrap text-red-500 ">
+    <div className="container mx-auto flex flex-wrap text-red-500 ">
       {data.map((item) => (
         <Link
           className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50"
@@ -17,12 +17,14 @@ const Product = () => {
             </div>
           )}
           {/* TEXT CONTAINER */}
-          <div className="flex items-center justify-between font-bold">
-            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
-            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
-              Add to Cart
-            </button>
+          <div className="flex items-center justify-start font-bold">
+            <h1 className="text-[12px] lg:text-xl uppercase p-2 mr-auto ">{item.title}</h1>
+            <div className="flex space-x-2">
+              <h2 className="text-3xl">${item.price}</h2>
+              <button className="text-xs md:text-[16px] lg:text-[20px] uppercase bg-red-500 text-white p-2 rounded-md ">
+                Add to Cart
+              </button>
+            </div>
           </div>
         </Link>
       ))}
